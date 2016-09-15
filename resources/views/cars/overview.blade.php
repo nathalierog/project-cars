16542eb0720c2c6e1d78d8c051b1e670620c32ff
ikedus <sander_m20@hotmail.com>
2016-09-15 13:40:54 +0200

    finished overview template

--- resources/views/cars/overview.blade.php
+++ resources/views/cars/overview.blade.php
@@ -4,42 +4,28 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12">
+        @for ($i = 0; $i < 25; $i++)
+        <a href="{{url('cars/details')}}">
             <div class="panel panel-default">
-                <div class="panel-heading">volkswekken gulf</div>
+                <div class="panel-heading"><h2 class="h4"><b>volkSWAGen gulf</b></h2></div>
 
                 <div class="panel-body">
                    <div class="col-md-4">
-                            <img class="img-responsive center-block" src="http://placehold.it/350x350" alt="plaatje">
-                    
-                    </div>
-                    <div class="col-md-8">
-                        <div class="col-md-12 col-sm-6">
-                            mooie waggie
-                        </div>
-                        <div class="col-md-4 col-sm-6 col-md-push-8">€54.000</div>
-                        <div class="col-md-8 col-sm-12 col-md-pull-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, similique aliquam velit ipsam.</div>
-                        
-                    </div> 
-                </div>
-            </div>
-            <div class="panel panel-default">
-                <div class="panel-heading">volkswekken gulf</div>
-
-                <div class="panel-body">
-                   <div class="col-md-4">
-                            <img class="img-responsive center-block" src="http://placehold.it/350x350" alt="plaatje">
+                            <img class="img-responsive center-block img-rounded" src="http://placehold.it/360x270" alt="plaatje">
                     
                     </div>
                     <div class="col-md-8">
                         <div class="col-md-12 col-xs-6">
-                            mooie waggie
+                            <b>03/2012</b>
                         </div>
-                        <div class="col-md-4 col-xs-6 col-md-push-8">€54.000</div>
-                        <div class="col-md-8 col-xs-12 col-md-pull-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, similique aliquam velit ipsam.</div>
+                        <div class="col-md-4 col-xs-6 col-md-push-8 text-right"><b>€54.000</b></div>
+                        <div class="col-md-8 col-sm-12 col-md-pull-4"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, similique aliquam velit ipsam.</p></div>
                         
                     </div> 
                 </div>
             </div>
+            </a>
+        @endfor
         </div>
     </div>
 </div>
