513504da904381c8fe6a6cbbb9896126c23d234e
PANNE-PC\Walter <99030353@mydavinci.nl>
2016-09-18 15:56:17 +0200

    Added responsive menu for mobile to main template

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,9 +1,22 @@
+function togglemobilemenu(){
+	if ($(window).width() >= 768){
+		$('#app-sticky-navbar').removeClass('navbar-fixed-top');
+		$('#app-sticky-navbar').affix({
+		  	offset: {
+		    	top: $('#app-default-navbar').height()
+		  	}
+		});
+	}
+	else{
+		$('#app-sticky-navbar').addClass('navbar-fixed-top').removeClass('affix-top');
+	}
+}
+
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
-
-    $('#app-sticky-navbar').affix({
-	  	offset: {
-	    	top: $('#app-default-navbar').height()
-	  	}
-	});
+    togglemobilemenu();
 });
+
+window.onresize = function(event){
+	togglemobilemenu();
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     @yield('header') 
 </head>
 <body id="app-layout">
-    <header class="navbar navbar-static-top" id="app-default-navbar">
+    <nav class="navbar navbar-static-top" id="app-default-navbar">
         <div class="container">
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
@@ -37,22 +37,46 @@
                 </ul>
             </div>
         </div>
-    </header>
-    <header class="navbar navbar-default static-top" id="app-sticky-navbar">
+    </nav>
+    <header class="navbar navbar-default" id="app-sticky-navbar">
         <div class="container">
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-mobile-menu">
                     <span class="sr-only">Open navigatie</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ URL::asset('img/logo.png') }}" alt="car logo" data-toggle="tooltip" data-placement="right" title="Klik hier om naar home te gaan">
+                    <img src="{{ URL::asset('img/logo.png') }}" alt="car logo">
                 </a>
             </div>
-            <div class="collapse navbar-collapse">
+            <div class="collapse navbar-collapse navbar-padding">
                 <ul class="nav navbar-nav navbar-right">
+                    <li><a href="{{ url('/zoeken') }}" data-toggle="tooltip" data-placement="left" title="Zoeken"><i class="fa fa-car fa-fw" aria-hidden="true"></i>Zoeken</a></li>
+                </ul>
+            </div>
+            <div class="collapse navbar-collapse" id="app-mobile-menu">
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/') }}" data-toggle="tooltip" data-placement="bottom" title="Home"><i class="fa fa-home fa-fw" aria-hidden="true"></i></a></li>
+                    <li><a href="{{ url('/about') }}" data-toggle="tooltip" data-placement="bottom" title="Over ons">Over ons</a></li>
+                    <li><a href="{{ url('/contact') }}" data-toggle="tooltip" data-placement="bottom" title="Contact"><i class="fa fa-phone fa-fw" aria-hidden="true"></i>Contact</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}" data-toggle="tooltip" data-placement="bottom" title="Inloggen"><i class="fa fa-sign-in fa-fw" aria-hidden="true"></i>Inloggen</a></li>
+                        <li><a href="{{ url('/register') }}" data-toggle="tooltip" data-placement="bottom" title="Registreren">Registreren</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Uitloggen</a></li>
+                            </ul>
+                        </li>
+                    @endif
                     <li><a href="{{ url('/zoeken') }}" data-toggle="tooltip" data-placement="left" title="Zoeken"><i class="fa fa-car fa-fw" aria-hidden="true"></i>Zoeken</a></li>
                 </ul>
             </div>
