f9c7205cdf7dcd87a1d76b1b292fa5b67a3e53f2
Nathalie Rog <99027109@mydavinci.nl>
2016-09-29 09:26:21 +0200

    Added about us page.

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="col-md-6">
+                <h4>
+                    <b>About</b>
+                </h4>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. 
+                    Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, 
+                    similique aliquam velit ipsam.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. 
+                    Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, 
+                    similique aliquam velit ipsam.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. 
+                    Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, 
+                    similique aliquam velit ipsam.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. 
+                    Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, 
+                    similique aliquam velit ipsam.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. 
+                    Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, 
+                    similique aliquam velit ipsam.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. 
+                    Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, 
+                    similique aliquam velit ipsam.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. 
+                    Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, 
+                    similique aliquam velit ipsam.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+                    Voluptatum recusandae nostrum unde fugiat officia accusamus sequi. 
+                    Voluptatem iure eius recusandae saepe vitae dolores ea, perferendis voluptas, 
+                    similique aliquam velit ipsam.
+                </p>
+            </div>
+            <div class="col-md-6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2474.1727486968553!2d4.030343715491864!3d51.674980929662944!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c45c21b2da6c0f%3A0x1e8175cc60ded0eb!2s4308+AE+Sirjansland!5e0!3m2!1snl!2snl!4v1474534998875" width="100%" height="300px" frameborder="0" style="border:0" allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
